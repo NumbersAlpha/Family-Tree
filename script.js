58205a842cf6391f2b1cb0280c41d1fbfa9a3c0e
NumbersAlpha <126000326+NumbersAlpha@users.noreply.github.com>
2023-04-28 09:55:48 -0400

    Added person A's parent depth
    for backup in case of replit crash

--- script.js
+++ script.js
@@ -8,37 +8,45 @@ class person {
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	background(100);
-  
+
   familyTree()
     }
 
 function familyTree()  {
 
-  
+
 	let Elisabeth = new person('Elisabeth', null)
-	
+
   let Marj = new person('Marj', Elisabeth)
 	let Rosanne = new person('Rosanne', Elisabeth)
-	let Ruthanne = new person('Ruthanne', Elisabeth)
-	let Donnie = new person('Donnie', Elisabeth)
-	let Patricia = new person('Patricia', Elisabeth)
-
-	let Kim = new person('Kim', Marj)
-
+  let Ruthanne = new person('Ruthanne', Elisabeth)
+  let Donnie = new person('Donnie', Elisabeth)
+  let Patricia = new person('Patricia', Elisabeth)
+  
+  let Kim = new person('null', Patricia)
   let John = new person('John', Kim)
 	let Jamie = new person('Jamie', Kim)
 
 	let people = [Elisabeth, Marj, Rosanne, Ruthanne, Donnie, Patricia, Kim, John, Jamie] 
 
   let A = random(people)
-
+  let B = random(people)
 
 let ALineage = A
 let generation = []
-  
+let ADepth = 0
+
   while (ALineage != null)  {
+    ADepth++
    generation.push(ALineage.name)
     ALineage = ALineage.parent 
   }
+
+if (A == B)  {
+  print('Person A and person B are the same person!')
+}
   print(generation)
+  print('Depth is ', ADepth)
+  
+  
 }
