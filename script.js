59fcb32ddd743b12152c5cd424f072736e023100
NumbersAlpha <126000326+NumbersAlpha@users.noreply.github.com>
2023-04-28 21:49:37 -0400

    Added more family members

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ function setup() {
 
 function familyTree() {
 
-
 	let Elisabeth = new person('Elisabeth', null)
 
 	let Marj = new person('Marj', Elisabeth)
@@ -23,11 +22,32 @@ function familyTree() {
 	let Donnie = new person('Donnie', Elisabeth)
 	let Patricia = new person('Patricia', Elisabeth)
 
-	let Kim = new person('Kim', Patricia)
-	let John = new person('John', Kim)
-	let Jamie = new person('Jamie', Kim)
+	let Kim = new person('Kim', Marj)
+ 	let Monica = new person('Monica', Marj)
+  	let Martin = new person('Martin', Marj)
+  	let Gabe = new person('Gabe', Marj)
+  	let John = new person('John', Marj)
+  	let Ben = new person('Ben', Marj)
+  	let Jerome = new person('Jerome', Marj)
+  
 
-	let people = [Elisabeth, Marj, Rosanne, Ruthanne, Donnie, Patricia, Kim, John, Jamie]
+	let Johnny = new person('Johnny', Kim)
+	let Jamie = new person('Jamie', Kim)
+  	let Julia = new person('Julia', Kim)
+  	let Phil = new person('Phil', Monica)
+  	let Alley = new person('Alley', Monica)
+  	let Anna = new person('Anna', Monica)
+  	let Steven = new person('Steven', Monica)
+  	let Ellane = new person('Ellane', Gabe)
+  	let Jake = new person('Jake', Gabe)
+  	let Veera = new person('Veera', Jerome)
+  	let Precila = new person('Precila', Jerome)
+  	let Clarence = new person('Clarence', Jerome)
+
+  	let Lincoln = new person('Lincoln', Phil)
+  	let Mo = new person('Mo', Ellane)
+
+let people = [Elisabeth, Marj, Rosanne, Ruthanne, Donnie, Patricia, Kim, Monica, Martin, Gabe, John, Ben, Jerome, John, Jamie, Julia, Phil, Alley, Anna, Steven, Ellane, Jake, Veera, Precila, Clarence]
 
 	let A = random(people)
 	let B = random(people)
@@ -43,12 +63,12 @@ function familyTree() {
 	}
 
 	let BFamily = B
-  let Bgeneration = []
+  	let Bgeneration = []
 	let BDepth = 0
 
 	while (BFamily != null) {
 		BDepth++
-    Bgeneration.push(BFamily.name)
+    		Bgeneration.push(BFamily.name)
 		BFamily = BFamily.parent
 	}
 
@@ -58,8 +78,8 @@ function familyTree() {
 
 	print('person A lineage is ', Ageneration)
 	print('A depth is ', ADepth)
-  print('person B lineage is ', Bgeneration)
-  print('B depth is ', BDepth)
+  	print('person B lineage is ', Bgeneration)
+  	print('B depth is ', BDepth)
 
 
   let ADiff = 0
@@ -68,8 +88,8 @@ function familyTree() {
 	for (let j = 0; j < BDepth; j++) {
 		for (let i = 0; i < ADepth; i++) {
 
-			if (Ageneration[i] == Bgeneration[j]) {
-        print(Ageneration[i],' is the most common ancestor')
+			if (A != B && Ageneration[i] == Bgeneration[j]) {
+        return print(Ageneration[i],' is the most common ancestor')
 			}
 		}
 	}
