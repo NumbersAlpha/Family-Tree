1c9450e12e6481db1bef9c1d3b5e1bb149b82936
NumbersAlpha <126000326+NumbersAlpha@users.noreply.github.com>
2023-04-28 23:05:53 -0400

    Added bare bones generation differences
    Generation differences between person A and person B with print identifiers

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function setup() {
 
 function familyTree() {
 
+
 	let Elisabeth = new person('Elisabeth', null)
 
 	let Marj = new person('Marj', Elisabeth)
@@ -23,74 +24,81 @@ function familyTree() {
 	let Patricia = new person('Patricia', Elisabeth)
 
 	let Kim = new person('Kim', Marj)
- 	let Monica = new person('Monica', Marj)
+  	let Monica = new person('Monica', Marj)
   	let Martin = new person('Martin', Marj)
   	let Gabe = new person('Gabe', Marj)
   	let John = new person('John', Marj)
-  	let Ben = new person('Ben', Marj)
-  	let Jerome = new person('Jerome', Marj)
+ 	let Ben = new person('Ben', Marj)
+ 	let Jerome = new person('Jerome', Marj)
   
 
 	let Johnny = new person('Johnny', Kim)
 	let Jamie = new person('Jamie', Kim)
-  	let Julia = new person('Julia', Kim)
+ 	let Julia = new person('Julia', Kim)
   	let Phil = new person('Phil', Monica)
   	let Alley = new person('Alley', Monica)
-  	let Anna = new person('Anna', Monica)
-  	let Steven = new person('Steven', Monica)
-  	let Ellane = new person('Ellane', Gabe)
-  	let Jake = new person('Jake', Gabe)
-  	let Veera = new person('Veera', Jerome)
-  	let Precila = new person('Precila', Jerome)
-  	let Clarence = new person('Clarence', Jerome)
+ 	let Anna = new person('Anna', Monica)
+ 	let Steven = new person('Steven', Monica)
+ 	let Ellane = new person('Ellane', Gabe)
+ 	let Jake = new person('Jake', Gabe)
+ 	let Veera = new person('Veera', Jerome)
+ 	let Precila = new person('Precila', Jerome)
+	let Clarence = new person('Clarence', Jerome)
 
   	let Lincoln = new person('Lincoln', Phil)
   	let Mo = new person('Mo', Ellane)
 
-let people = [Elisabeth, Marj, Rosanne, Ruthanne, Donnie, Patricia, Kim, Monica, Martin, Gabe, John, Ben, Jerome, John, Jamie, Julia, Phil, Alley, Anna, Steven, Ellane, Jake, Veera, Precila, Clarence]
+	let people = [Elisabeth, Marj, Rosanne, Ruthanne, Donnie, Patricia, Kim, Monica, Martin, Gabe, John, Ben, Jerome, John, Jamie, Julia, Phil, Alley, Anna, Steven, Ellane, Jake, Veera, Precila, Clarence]
 
 	let A = random(people)
 	let B = random(people)
 
-	let AFamily = A
-	let Ageneration = []
+	let personA = A
+	let ALineage = []
 	let ADepth = 0
 
-	while (AFamily != null) {
+	while (personA != null) {
 		ADepth++
-		Ageneration.push(AFamily.name)
-		AFamily = AFamily.parent
+		ALineage.push(personA.name)
+		personA = personA.parent
 	}
 
-	let BFamily = B
-  	let Bgeneration = []
+	let personB = B
+  	let BLineage = []
 	let BDepth = 0
 
-	while (BFamily != null) {
+	while (personB != null) {
 		BDepth++
-    		Bgeneration.push(BFamily.name)
-		BFamily = BFamily.parent
+    		BLineage.push(personB.name)
+		personB = personB.parent
 	}
-
+  
+	print('person A lineage is ', ALineage)
+	print('A depth is ', ADepth)
+  	print('person B lineage is ', BLineage)
+  	print('B depth is ', BDepth)
+  
 	if (A == B) {
 		print('Person A and person B are the same person!')
 	}
-
-	print('person A lineage is ', Ageneration)
-	print('A depth is ', ADepth)
-  	print('person B lineage is ', Bgeneration)
-  	print('B depth is ', BDepth)
-
-
+else  {
   let ADiff = 0
   let BDiff = 0
 
 	for (let j = 0; j < BDepth; j++) {
+   	BDiff++
 		for (let i = 0; i < ADepth; i++) {
+      	ADiff++
+			if (A != B && ALineage[i] == BLineage[j]) {
+        print('Person A is connected to person B by their generation ', ADiff)
+        print('Person B is connected to person A by their generation ', BDiff)
 
-			if (A != B && Ageneration[i] == Bgeneration[j]) {
-        return print(Ageneration[i],' is the most common ancestor')
+        // if (ADiff == BDiff)
+        
+        return print(ALineage[i],' is the most common ancestor')
 			}
 		}
+  ADiff = 0
+	}
 	}
 }
